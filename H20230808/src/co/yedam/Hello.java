fa7e53cd759778d2d8e87eab60c7ef35e0261ac2
admin <admin@YD5-06>
2023-08-08 11:06:45 +0900

    first  commit:

---
+++ H20230808/src/co/yedam/Hello.java
@@ -0,0 +1,11 @@
+package co.yedam;
+
+public class Hello {
+	public static void main(String [] args) {	//함수안에 속한 함수를 메소드라 한다.	
+		System.out.println("안녕하세요...");
+	}
+}
+
+// 소스 코드가 실행되는것이 아니라 빌드된 클래스 파일이 실행됨
+// class 파일이 삭제 되었을 시 clean 실행 후 자동 빌드되어 class 파일이 생성됨
+// https://github.com/eunkyoji/java_project.git - GIT Hub
