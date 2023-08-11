84e995be17411841ee954ece7f93784ddbe55f12
eunku <eunku@DESKTOP-QOPKF9B>
2023-08-11 22:50:56 +0900

    게시판

---
+++ H20230809/src/co/yedam/classes/board/Board.java
@@ -0,0 +1,65 @@
+package co.yedam.classes.board;
+
+public class Board {
+	private int		no;
+	private String	title;
+	private String	content;
+	private String	name;
+	private String	date;
+	
+	public Board(int no, String title, String content, String name, String date) {
+		super();
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.name = name;
+		this.date = date;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [no=" + no + ", title=" + title + ", name=" + name + ", date=" + date + "]"
+				+ "\n content \n" + content;
+	}
+	
+}
